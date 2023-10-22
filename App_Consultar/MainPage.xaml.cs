c536a076e2a6f8d6f47bbc3640cf08fdbc65d148
YBD-SOFT <131542050+Yomar2023-eng@users.noreply.github.com>
2023-10-22 17:53:41 -0500

    💥💉�Yomar BD💉💉💉💥

---
+++ App_Consultar/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_Consultar
+{
+    public partial class MainPage : ContentPage
+    {
+        ApisPeru apisPeru = new ApisPeru(); 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ver_Clicked(object sender, EventArgs e)
+        {
+            ConsultarCliente();
+
+        }
+
+        private void ConsultarCliente()
+        {
+            try
+            {
+                if (ConsultarDoc.Text.Length == 11)
+                {
+                    dynamic respuesta = apisPeru.Get("https://api.apis.net.pe/v1/ruc?numero=" + ConsultarDoc.Text + "");
+                
+                    Nombre.Text = respuesta.nombre.ToString();
+                    apPaterno.Text = respuesta.estado.ToString();
+                    apMaterno.Text = respuesta.condicion.ToString();
+
+
+                }
+
+                if (ConsultarDoc.Text.Length == 8)
+                {
+                    dynamic respuesta = apisPeru.Get("https://api.apis.net.pe/v1/dni?numero=" + ConsultarDoc.Text + "");
+
+                    Nombre.Text = respuesta.nombres.ToString();
+                    apPaterno.Text = respuesta.apellidoPaterno.ToString();
+                    apMaterno.Text = respuesta.apellidoMaterno.ToString();
+
+
+                }
+
+
+            }
+            catch
+
+            (Exception ex)
+            { }
+        }
+
+    }
+    
+}
